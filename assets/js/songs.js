c1d9e5974ceca69525f848d4c0e2eec8969ea702
Kyle Perkins <kyperkins@gmail.com>
2023-12-26 03:19:26 -0500

    finish

---
+++ assets/js/songs.js
@@ -0,0 +1,81 @@
+const songs = [
+    {
+        path: 'assets/mp3/baby-mandala-169039.mp3',
+        displayName: 'Baby Mandala',
+        cover: 'assets/img/baby mandala.jpg',
+        artist: 'Praz  Khanal',
+        // Music by <a href="https://pixabay.com/users/prazkhanal-24653570/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=169039">Praz Khanal</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=169039">Pixabay</a>
+
+    },
+    {
+        path: 'assets/mp3/cinim-brainfluid-122844.mp3',
+        displayName: 'Brain Fluid',
+        cover: 'assets/img/brain fluid.jpg',
+        artist: 'CINIM',
+        //Music by <a href="https://pixabay.com/users/cinim-30594952/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=122844">Molina</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=122844">Pixabay</a>
+
+    },
+    {
+        path: 'assets/mp3/drive-breakbeat-173062.mp3',
+        displayName: 'Drive Breakbeat',
+        cover: 'assets/img/drive breakbeat.jpg',
+        artist: 'Rockot',
+        // Music by <a href="https://pixabay.com/users/rockot-1947599/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=173062">Rockot</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=173062">Pixabay</a>
+
+    },
+    {
+        path: 'assets/mp3/for-future-base-159125.mp3',
+        displayName: 'For Future Base',
+        cover: 'assets/img/for future base.jpg',
+        artist: 'The_Mountain',
+        // Music by <a href="https://pixabay.com/users/the_mountain-3616498/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=159125">Dmitrii Kolesnikov</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=159125">Pixabay</a>
+
+    },
+    {
+        path: 'assets/mp3/goldn-116392.mp3',
+        displayName: 'Goldn',
+        cover: 'assets/img/goldn.jpg',
+        artist: 'Praz  Khanal',
+        // Music by <a href="https://pixabay.com/users/prazkhanal-24653570/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=169039">Praz Khanal</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=169039">Pixabay</a>
+
+    },
+    {
+        path: 'assets/mp3/leonell-cassio-the-blackest-bouquet-118766.mp3',
+        displayName: 'The Blackest Bouquet',
+        cover: 'assets/img/the blackest bouquet.jpg',
+        artist: 'LeonellCassio',
+        // Music by <a href="https://pixabay.com/users/leonellcassio-2264996/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=118766">Leonell Cassio</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=118766">Pixabay</a>
+
+    },
+    {
+        path: 'assets/mp3/leonell-cassio-the-paranormal-is-real-ft-carrie-163742.mp3',
+        displayName: 'The Paranormal is Read',
+        cover: 'assets/img/the paranormal is real.jpg',
+        artist: 'LeonellCassio',
+        // Music by <a href="https://pixabay.com/users/leonellcassio-2264996/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=118766">Leonell Cassio</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=118766">Pixabay</a>
+
+    },
+    {
+        path: 'assets/mp3/once-in-paris-168895.mp3',
+        displayName: 'Once in Paris',
+        cover: 'assets/img/once in paris.jpg',
+        artist: 'Pumpupthemind',
+        // Music by <a href="https://pixabay.com/users/pumpupthemind-19969411/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=168895">Pumpupthemind</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=168895">Pixabay</a>
+
+    },
+    {
+        path: 'assets/mp3/tvari-tokyo-cafe-159065.mp3',
+        displayName: 'Tpokyo Cafe',
+        cover: 'assets/img/tokyo cafe.jpg',
+        artist: 'TVARI',
+        // Music by <a href="https://pixabay.com/users/tvari-38302100/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=159065">TVARI</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=159065">Pixabay</a>
+
+    },
+    {
+        path: 'assets/mp3/unlock-me-149058.mp3',
+        displayName: 'Unlock Me',
+        cover: 'assets/img/unlock-me.jpg',
+        artist: 'Praz Khanal',
+        // Music by <a href="https://pixabay.com/users/prazkhanal-24653570/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=149058">Praz Khanal</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=149058">Pixabay</a>
+    }
+]
